2fa1fc1877197c7a21c0e475d514967d375a7cdc
vasily ryabtsev <vasilyryabtsev@gmail.com>
2026-02-21 04:03:43 +0300

    feat: add import

--- src/trainer/__init__.py
+++ src/trainer/__init__.py
@@ -1,3 +1,4 @@
 from src.trainer.inferencer import Inferencer
 from src.trainer.trainer import Trainer
+from src.trainer.vocos_inferencer import VocosInferencer
 from src.trainer.vocos_trainer import VocosTrainer
