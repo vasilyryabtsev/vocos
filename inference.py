a22c62ec050554adb21fb2e597eab1e3b206dca9
vasily ryabtsev <vasilyryabtsev@gmail.com>
2026-02-21 15:00:21 +0300

    fix: output dir hardcode

--- inference.py
+++ inference.py
@@ -44,7 +44,7 @@ def main(config):
         )
 
     # save_path for model predictions
-    save_path = ROOT_PATH / "data" / "saved" / config.inferencer.save_path
+    save_path = config.inferencer.save_path
     save_path.mkdir(exist_ok=True, parents=True)
 
     inferencer = VocosInferencer(
